e6b11dc353e22d1263afd1ae6a78e25b090cc6cd
carlos <carlos.melgarejo.solorzano@hotmail.com>
2023-08-26 18:29:15 -0500

    añadiendo angular material2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthRoutingModule } from './auth/auth.routes';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    AuthRoutingModule
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }

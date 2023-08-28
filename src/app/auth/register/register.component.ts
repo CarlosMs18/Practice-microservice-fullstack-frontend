f021d99c978ebc9c57b967c34fffa5ccbde1abc7
carlos <carlos.melgarejo.solorzano@hotmail.com>
2023-08-27 19:14:57 -0500

    añadiendo html de register

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+
+
+import { ToastrService } from 'ngx-toastr'
 
 @Component({
   selector: 'app-register',
@@ -7,9 +13,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  public registerForm : FormGroup = this.fb.group({
+    username : ['',[Validators.required, Validators.minLength(4)]],
+    password : ['',Validators.required],
+    confirmPassword : ['',Validators.required],
+    nombre : ['',Validators.required],
+    apellido : ['',Validators.required],
+    email : ['',Validators.required],
+    telefono : ['',Validators.required],
+  })
+  constructor(
+    private fb: FormBuilder,
+    private router :Router,
+    private authService : AuthService,
+    private toastr : ToastrService
+
+  ) { }
 
   ngOnInit(): void {
   }
 
+
+  register(){
+    if(this.registerForm.invalid){
+      this.registerForm.markAllAsTouched();
+      return;
+    }
+  }
 }
